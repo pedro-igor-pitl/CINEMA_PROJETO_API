26adc76a11ab1853d4a50b9c759aa9d39f9ad1e7
piet2b <piet2bezerra+dev@gmail.com>
2024-10-16 18:34:49 -0300

    Edit

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -6,12 +6,14 @@ from .models import Usuario, Filme
 
 def create_app():
     app = Flask(__name__)
+    senha = 'Souz@105696'
+    # Configurações do banco de dados PostgreSQL105696localhost
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{senha}@localhost:5432/cinema_api'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Souz@localhost:5432/cinema_api'
 
-    # Configurações do banco de dados PostgreSQL
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pedroBD@localhost:5432/cinema_projeto_api'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    app.config['SECRET_KEY'] = '2893cbbe206d16deedaa11ccdc9790a6'
+    app.config['SECRET_KEY'] = '9d6a0f900ca80f21e659c6bcba698913'
     # Inicializa o SQLAlchemy com o app Flask
     db.init_app(app)
 
--- back-end/app/models.py
+++ back-end/app/models.py
@@ -17,3 +17,33 @@ class Filme(db.Model):
     duracao = db.Column(db.String, nullable=False)
     genero = db.Column(db.String(100), nullable=False)
     data_lancamento = db.Column(db.String(20))
+
+class Ingresso(db.Model):
+    __tablename__ = 'ingresso'
+    id_ingresso = db.Column(db.Integer, primary_key=True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'), nullable=False)
+    id_sala = db.Column(db.Integer, db.ForeignKey('sala.id_sala'), nullable=False)
+    id_poltrona = db.Column(db.Integer, db.ForeignKey('poltrona.id_poltrona'), nullable=False)
+    qrcode = db.Column(db.String(255), nullable=True)
+    data_pedido = db.Column(db.String, nullable=True)
+
+class Poltrona(db.Model):
+    __tablename__ = 'poltrona'
+    id_poltrona = db.Column(db.Integer, primary_key=True)
+    posicao = db.Column(db.String(50), nullable=False)
+    tipo_poltrona = db.Column(db.String(50), nullable=False)
+    id_sala = db.Column(db.Integer, db.ForeignKey('sala.id_sala'), nullable=False)
+
+class Sessao(db.Model):
+    __tablename__ = 'sessao'
+    id_sessao = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(20), nullable=False)
+    id_sala = db.Column(db.Integer, db.ForeignKey('sala.id_sala'), nullable=False)
+    preco = db.Column(db.Numeric(10, 2), nullable=False)
+    linguagem = db.Column(db.String(50), nullable=False)
+
+class Sala(db.Model):
+    __tablename__ = 'sala'
+    id_sala = db.Column(db.Integer, primary_key=True)
+    qt_poltrona = db.Column(db.Integer, nullable=False)
+    id_sessao = db.Column(db.Integer, db.ForeignKey('sessao.id_sessao'), nullable=False)
