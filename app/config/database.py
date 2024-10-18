f271c9e224a85356c7c71128015ffdfebe985ac6
piet2b <piet2bezerra+dev@gmail.com>
2024-10-18 18:42:17 -0300

    Organização

--- app/config/database.py
+++ app/config/database.py
@@ -6,13 +6,13 @@ db = SQLAlchemy()
 def createapp():
     app = Flask(__name__)
 
-    senha = 'pedroBD'
-    # Configurações do banco de dados PostgreSQL
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{senha}@localhost:5432/cinema_projeto_api'
-    print("Chegou aqui")
+    senha = 'Souz@105696'
+    # Configurações do banco de dados PostgreSQL105696localhost
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{senha}@localhost:5432/cinema_api'
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    app.config['SECRET_KEY'] = '2893cbbe206d16deedaa11ccdc9790a6'
+    app.config['SECRET_KEY'] = '9d6a0f900ca80f21e659c6bcba698913'
     # Inicializa o SQLAlchemy com o app Flask
     db.init_app(app)
 
